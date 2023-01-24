e414171fff22c45ec36eb046f594dad91eab2359
Gabriel Augusto <61918927+gabriel-adsv@users.noreply.github.com>
2023-01-24 00:00:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,24 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // // pegar a tag img
-  // const img = document.querySelector("#profile img")
+  // pegar a tag img
+  const img = document.querySelector("#change-img img")
 
-  // // substituir a imagem
-  // if (html.classList.contains("light")) {
-  //   // se tiver light mode, adicionar a imagem light
-  //   img.setAttribute("src", "./assets/avatar-light.jpeg")
-  // } else {
-  //   // se tiver sem light mode, manter a imagem normal
-  //   img.setAttribute("src", "./assets/avatar.png")
-  // }
-
-  // // substituir o ícone do skoob
-  // if (html.classList.contains("light")) {
-  //   // se tiver light mode, adicionar a imagem light
-  //   img.setAttribute("src", "./assets/icons/skoob-light.svg")
-  // } else {
-  //   // se tiver sem light mode, manter a imagem normal
-  //   img.setAttribute("src", "./assets/icons/skoob.svg")
-  // }
+  // substituir o ícone do skoob
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "icons/skoob-light.svg")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "icons/skoob.svg")
+  }
 }
